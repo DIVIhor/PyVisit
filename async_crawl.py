656ec977176ee8c7f24787f0154def2f5343a787
DIVIhor <counter088@gmail.com>
2025-11-06 20:36:48 +0000

    feat: Add a limit for pages to crawl. Make max concurrency and max pages to crawl configurable via CLI.

--- async_crawl.py
+++ async_crawl.py
@@ -1,5 +1,5 @@
 import asyncio
-from asyncio import Lock, Semaphore
+from asyncio import Lock, Semaphore, Task
 
 from urllib.parse import urlparse
 
@@ -8,11 +8,11 @@ from aiohttp import ClientSession
 
 from crawl import extract_page_data, normalize_url
 
-from config import MAX_CONCURRENCY
-
 
 class AsyncCrawler:
-    def __init__(self, base_url: str, max_concurrency: int):
+    def __init__(
+        self, base_url: str, max_concurrency: int, max_pages_to_crawl: int
+    ):
         self.base_url = base_url
         self.base_domain: str = urlparse(self.base_url).netloc
         self.page_data: dict[str, dict[str, str | list[str]]] = {}
@@ -20,6 +20,11 @@ class AsyncCrawler:
         self.max_concurrency = max_concurrency
         self.semaphore: Semaphore = asyncio.Semaphore(self.max_concurrency)
         self.session: ClientSession
+
+        # crawling control for maximum pages
+        self.max_pages: int = max_pages_to_crawl
+        self.should_stop: bool = False
+        self.all_tasks: set[Task] = set()
 
     async def __aenter__(self):
         """Open a client session"""
@@ -30,9 +35,23 @@ class AsyncCrawler:
         """Close the client session"""
         await self.session.close()
 
-    async def add_page_visit(self, normalized_url: str):
-        """Check if the page was visited earlier"""
+    async def add_page_visit(self, normalized_url: str) -> bool:
+        """Check if the page was visited earlier or reached crawling maximum"""
+        # check if crawler should stop
+        if self.should_stop:
+            return True
+
         async with self.lock:
+            # check if reached max pages limit
+            if len(self.page_data) == self.max_pages:
+                self.should_stop = True
+                print("Reached maximum number of pages to crawl.")
+                for task in self.all_tasks:
+                    if not task.done():
+                        task.cancel()
+                return True
+
+            # check if page is already visited
             return normalized_url in self.page_data
 
     async def get_html(self, url: str) -> str:
@@ -61,10 +80,14 @@ class AsyncCrawler:
 
     async def crawl_page(self, current_url: str):
         """Recursively traverse found URLs"""
+        # stop further crawling if reached maximum crawls
+        if self.should_stop:
+            return
+
         # make sure the current url is on the same domain as the base url
         if urlparse(current_url).netloc != self.base_domain:
             return
-        # ignore already crawled pages
+        # ignore already crawled pages or links beyond the limit
         if await self.add_page_visit(
             normalized_url := normalize_url(current_url)
         ):
@@ -86,14 +109,19 @@ class AsyncCrawler:
                 self.page_data[normalized_url] = extract_page_data(
                     html, current_url
                 )
+                # save links for processing without locks
+                links: list[str] = self.page_data[normalized_url][
+                    "outgoing_links"
+                ]  # type:ignore
 
-                # schedule to crawl each URL on the page
-                tasks: list[asyncio.Task] = [
-                    asyncio.create_task(self.crawl_page(current_url=url))
-                    for url in self.page_data[normalized_url]["outgoing_links"]
-                ]
-
-        await asyncio.gather(*tasks)
+        try:
+            # schedule crawling for each URL on the page
+            for url in links:
+                self.all_tasks.add(asyncio.create_task(self.crawl_page(url)))
+            await asyncio.gather(*self.all_tasks)
+        finally:
+            self.all_tasks.clear()
+            return
 
     async def crawl(self) -> dict[str, dict[str, str | list[str]]]:
         """Start crawling from `base_url` and return the page data"""
@@ -103,9 +131,13 @@ class AsyncCrawler:
 
 async def crawl_site_async(
     base_url: str,
+    max_concurrency: int,
+    max_pages_to_crawl: int,
 ) -> dict[str, dict[str, str | list[str]]]:
     """Create an `AsyncCrawler`'s instance on the `base_url`
     and start crawling
     """
-    async with AsyncCrawler(base_url, MAX_CONCURRENCY) as crawler:
+    async with AsyncCrawler(
+        base_url, max_concurrency, max_pages_to_crawl
+    ) as crawler:
         return await crawler.crawl()
--- main.py
+++ main.py
@@ -3,21 +3,31 @@ import sys
 
 from async_crawl import crawl_site_async
 
+from config import MAX_CONCURRENCY, MAX_PAGES_TO_CRAWL
+
 
 async def main():
     # input control
     if (arguments := len(sys.argv)) < 2:
         print("no website provided")
         sys.exit(1)
-    elif arguments > 2:
+    elif arguments > 4:
         print("too many arguments provided")
         sys.exit(1)
+
+    # set up limits
+    max_concurrency: int = (
+        int(sys.argv[2]) if arguments == 4 else MAX_CONCURRENCY
+    )
+    max_pages_to_crawl: int = (
+        int(sys.argv[3]) if arguments == 4 else MAX_PAGES_TO_CRAWL
+    )
 
     print(f"starting crawl of: {(base_url := sys.argv[1])}")
 
     # fetch HTML
     page_data: dict[str, dict[str, str | list[str]]] = await crawl_site_async(
-        base_url
+        base_url, max_concurrency, max_pages_to_crawl
     )
     print("\n", f"Found {len(page_data)} pages:")
     for page in page_data.values():
